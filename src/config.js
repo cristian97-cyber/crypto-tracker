2fa375976f32d634161dbf8805793a6dc8bfebac
cristian97-cyber <cristianesposito961@gmail.com>
2022-02-07 15:33:58 +0100

    Updated .gitignore

---
+++ src/config.js
@@ -0,0 +1,22 @@
+const STATS_API_URL = "https://api.coinlore.net/api/global/";
+const COINS_API_URL =
+	"https://api.coinstats.app/public/v1/coins?skip=0&currency=USD";
+const COIN_API_URL = "https://api.coinstats.app/public/v1/coins";
+const MARKETS_API_URL = "https://api.coinstats.app/public/v1/markets";
+const EXCHANGES_API_URL = "https://api.coinlore.net/api/exchanges/";
+const HISTORICAL_API_URL = "https://api.coinpaprika.com/v1/coins";
+const NEWS_API_URL =
+	"https://newsdata.io/api/1/news?apikey=pub_3961f5d294beabc0d49222f79f7abd8f442c&q=crypto&language=en";
+const NEWS_DB_URL =
+	"https://crypto-tracker-2a483-default-rtdb.firebaseio.com/news.json";
+
+export {
+	STATS_API_URL,
+	COINS_API_URL,
+	COIN_API_URL,
+	MARKETS_API_URL,
+	EXCHANGES_API_URL,
+	HISTORICAL_API_URL,
+	NEWS_API_URL,
+	NEWS_DB_URL,
+};
